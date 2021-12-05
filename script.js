3573ff053739163a86cd31bbcac04fe597f103db
Jay-zar <95107672+Jay-zar@users.noreply.github.com>
2021-12-05 17:16:10 -0500

    Try a console log in getapi

--- script.js
+++ script.js
@@ -1,47 +1,48 @@
 // api url
-const api_url = 
-      "https://employeedetails.free.beeceptor.com/my/api/path";
-  
+const api_url =
+	"https://swapi.dev/api/planets/2/";
+
 // Defining async function
 async function getapi(url) {
-    
-    // Storing response
-    const response = await fetch(url);
-    
-    // Storing data in form of JSON
-    var data = await response.json();
-    console.log(data);
-    if (response) {
-        hideloader();
-    }
-    show(data);
+	
+	// Storing response
+	const response = await fetch(url);
+	
+	// Storing data in form of JSON
+    console.log("did I print?");
+	var data = await response.json();
+	console.log(data);
+	if (response) {
+		hideloader();
+	}
+	show(data);
 }
 // Calling that async function
 getapi(api_url);
-  
+
 // Function to hide the loader
 function hideloader() {
-    document.getElementById('loading').style.display = 'none';
+	document.getElementById('loading').style.display = 'none';
 }
 // Function to define innerHTML for HTML table
 function show(data) {
-    let tab = 
-        `<tr>
-          <th>Name</th>
-          <th>Office</th>
-          <th>Position</th>
-          <th>Salary</th>
-         </tr>`;
-    
-    // Loop to access all rows 
-    for (let r of data.list) {
-        tab += `<tr> 
-    <td>${r.name} </td>
-    <td>${r.office}</td>
-    <td>${r.position}</td> 
-    <td>${r.salary}</td>          
+	let tab =
+		`<tr>
+		<th>Name</th>
+		<th>Office</th>
+		<th>Position</th>
+		<th>Salary</th>
+		</tr>`;
+	
+	// Loop to access all rows
+	for (let r of data.list) {
+		tab += `<tr>
+	<td>${r.name} </td>
+	<td>${r.office}</td>
+	<td>${r.position}</td>
+	<td>${r.salary}</td>		
 </tr>`;
-    }
-    // Setting innerHTML as tab variable
-    document.getElementById("employees").innerHTML = tab;
+	}
+	// Setting innerHTML as tab variable
+	document.getElementById("employees").innerHTML = tab;
 }
