4dd5e3da6e787e39b3e21a4dfcc4aa6d6901b4a9
Jayzar <jayzar.lynn@gmail.com>
2021-12-08 17:20:09 -0500

    JS adjustments

--- planets.js
+++ planets.js
@@ -1,61 +1,75 @@
 const apiPlanets = "https://swapi.dev/api/planets/"
-// const planetParams =
 
 let params = new URLSearchParams(document.location.search);
-    const numberCheck = params.get("apiNumber");
-    console.log(numberCheck);
+const numberCheck = params.get("apiNumber");
 
-console.log()
-function fetchData() {
-    fetch("http://swapi.dev/api/planets/"+numberCheck).then(response => {
-        console.log(response);
+//async call to get the names of the residents from their respective endpoints
+async function fetchCharacter(residentEndpoints) {
+    let response = await fetch(residentEndpoints);
+    let data = await response.json();
+    return data;
+}
+
+
+ async function fetchData() {
+    fetch("http://swapi.dev/api/planets/"+numberCheck)
+    .then(response => {
         if(!response.ok) {
             throw Error("error")
         }
         return response.json(); 
+    })
+    .then(jsonResponse => {
+        let characterPromises = [];
 
-    }).then(data => {
-       
-       // console.log(Object.values(data));
-        // data test
-      //  console.log(data);
-        let residentsHTML = ``;
+        // Getting resident information
+        for (let i = 0; i < jsonResponse.residents.length; i++) {
+            const characterPromise = fetchCharacter(jsonResponse.residents[i]);
+            characterPromises.push(characterPromise)
+        }
 
-    // checking if planet has residents
-      for (let i = 0; i < data.residents.length; i++) {
-            console.log(data.residents[i]);
-          residentsHTML+= `${data.residents[i]}<br>`
-      }
-
-      let residentText = ``;
-        //add the correct residents text to put into HTML
-      if (data.residents.length>0) {
-          residentText= "<p>Residents: ";
-      }
-      else {
-          residentText= "<p>Residents: None"
-      }
-      //converting object
-        const html = `<div class= "infoSet">
-        <h2>Planet ${data.name}</h2>
-        <p>Rotational Period: ${data.rotation_period}</p>
-        <p>Orbital Period: ${data.orbital_period}</p>
-        <p>Diameter: ${data.diameter}</p>
-        <p>Climate: ${data.climate}</p>
-        <p>Gravity: ${data.gravity}</p>
-        <p>Terrain: ${data.terrain}</p>
-        <p>Surface Water: ${data.surface_water}</p>
-        <p>Population: ${data.population}</p>
-        ${residentText}${residentsHTML}</p>
-        `
-
-      
-       // console.log(html); //html that should be visible
-        document.querySelector('#info-sidebar').insertAdjacentHTML("afterbegin", html)
+        Promise.all(characterPromises)
+        .then(characters => {
+            let residentsHTML = ``;
+            for (let i = 0; i < characters.length; i++) {
+                const character = characters[i]
+                const split_url = character.url.split("/");
+                const id = split_url[split_url.length-2];
+                residentsHTML+= `<a href="people.html?apiNumber=${id}">${character.name}</a><br>`
+            }
+            let residentText = ``;
+            //add the correct residents text to put into HTML
+            if (jsonResponse.residents.length > 0) {
+                residentText = `<p class="peopleP">Residents: `;
+            }
+            else {
+                residentText = `<p>Residents: None`
+            }
+    
+            //converting object
+            const html = `<div class= "infoSet">
+            <h2>Planet ${jsonResponse.name}</h2>
+            <p>Rotational Period: ${jsonResponse.rotation_period}</p>
+            <p>Orbital Period: ${jsonResponse.orbital_period}</p>
+            <p>Diameter: ${jsonResponse.diameter}</p>
+            <p>Climate: ${jsonResponse.climate}</p>
+            <p>Gravity: ${jsonResponse.gravity}</p>
+            <p>Terrain: ${jsonResponse.terrain}</p>
+            <p>Surface Water: ${jsonResponse.surface_water}</p>
+            <p>Population: ${jsonResponse.population}</p>
+            ${residentText}${residentsHTML}</p>
+            `
+            //adding info to HTML div
+            document.querySelector('#info-sidebar').insertAdjacentHTML("afterbegin", html)
+        })
+        .catch(function (error) {
+            const errorHTML = `<p>There was an error processing your request, try refreshing the page</p>`
+            document.querySelector('#info-sidebar').insertAdjacentHTML("afterbegin", errorHTML)
+        })
     }).catch(function (error) {
-            console.log(error);
-        });
+        const errorHTML = `<p>We couldn't find what you were looking for.</p>`
+        document.querySelector('#info-sidebar').insertAdjacentHTML("afterbegin", errorHTML)
+    });
 }
-  
-  console.log("End of functions");
-  fetchData(); 
+
+fetchData(); 
